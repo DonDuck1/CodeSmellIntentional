f74bcaac97647baefa8f095cfcedbff08e6f739d
DonDuck1 <malj0042@hz.nl>
2023-11-30 10:05:47 +0100

    Have fun! :)

---
+++ src/edu/hz/Main.java
@@ -0,0 +1,72 @@
+package edu.hz;
+
+import edu.hz.io.*;
+import edu.hz.statement.HZMissionStatement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // define
+        ConsoleWriter writer = new ConsoleWriter();
+        ConsoleReader reader = new ConsoleReader();
+        // execute
+        HZMissionStatement HZIP = new HZMissionStatement();
+        writer.writeLines(HZIP.getFirstAmbition());
+        boolean ILikeTurtles = true;
+        int johnnieJack = 32;
+        int nm1 = Integer.parseInt("-1"); // set variable for number
+        int n0 = Integer.parseInt("0"); // set variable for number
+        int n1 = Integer.parseInt("2"); // set variable for number
+        int n2 = Integer.parseInt("3"); // set variable for number
+        int n3 = Integer.parseInt("4"); // set variable for number
+        int n4 = Integer.parseInt("5"); // set variable for number
+        int n5 = Integer.parseInt("6"); // set variable for number
+        int n6 = Integer.parseInt("7"); // set variable for number
+        int n7 = Integer.parseInt("8"); // set variable for number
+        int n8 = Integer.parseInt("9"); // set variable for number
+        int n9 = Integer.parseInt("10"); // set variable for number
+        int n10 = Integer.parseInt("11"); // set variable for number
+        int n11 = Integer.parseInt("12"); // set variable for number
+        int n12 = Integer.parseInt("13"); // set variable for number
+        int n13 = Integer.parseInt("14"); // set variable for number
+        int n14 = Integer.parseInt("15"); // set variable for number
+        int n15 = Integer.parseInt("16"); // set variable for number
+        int n16 = Integer.parseInt("17"); // set variable for number
+        int n17 = Integer.parseInt("18"); // set variable for number
+        int n18 = Integer.parseInt("19"); // set variable for number
+        int n19 = Integer.parseInt("20"); // set variable for number
+        int n20 = Integer.parseInt("21"); // set variable for number
+        if (false) {
+        writer.writeLine("Hello");
+        } else if (false) {
+        writer.writeLine("Hello");
+        }
+        n1 = 1;
+        n19 = 20;
+        if (Math.floor(Math.random()*(n19- -n1+n1)+ -n1)+Math.floor(Math.random()*(n19- -n1+n1)+ -n1)>n19) { // Select random numbers
+        if (!ILikeTurtles == true || ILikeTurtles == false) { // This does... something
+        if (ILikeTurtles) { // This does... something
+        writer.writeLine("Hello");
+        } else if (johnnieJack < 1000000000) { // This does... something
+        writer.writeLine("Hello");
+        }
+        }
+        if (ILikeTurtles == true) { // This does... something
+        if (johnnieJack > 20) { // This does... something
+        writer.writeLine("Hello");
+        }
+        }
+        if (ILikeTurtles == false) { // This does... something
+        if (true) { // This does... something
+        writer.writeLine("Hello");
+        }
+        }
+        }
+        else if (Math.floor(Math.random()*(n19- -n1+n1)+ -n1)+Math.floor(Math.random()*(n19- -n1+n1)+ -n1)>n4) { // This does... something
+        writer.writeLine("Hello");
+        }
+        else { // This does... something
+        writer.writeLine("Hello");
+        }
+    }
+}
